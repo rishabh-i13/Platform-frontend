f88b4b2d62d13e488019138250959246c0b6618b
Satyam <satyambharti080@gmail.com>
2024-08-15 21:51:35 +0530

    Styling changes made

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,102 +1,57 @@
 import React from "react";
 
 const Posts = () => {
+  const blogs = [
+    {
+      imgUrl: "https://th-i.thgim.com/public/incoming/8y78id/article68527890.ece/alternates/LANDSCAPE_1200/PTI08_15_2024_000033B.jpg",
+      topic: "In his 11th I-day speech, PM Modi flags ‘secular civil code’,women’s safety, exudes pragmatism on Bangladesh",
+      content: "In his 11th successive Independence Day address to the nation from the ramparts of the Red Fort, Prime Minister Narendra Modi gave a renewed push to the BJP’s ideological aim of a Uniform Civil Code, reframing the idea as a “secular civil code” enshrined in the Constitution and a way of ensuring equality before law for all.",
+      author: "Iron Man",
+      date: "12th April 2024"
+    },
+    {
+      imgUrl: "https://th-i.thgim.com/public/incoming/8y78id/article68527890.ece/alternates/LANDSCAPE_1200/PTI08_15_2024_000033B.jpg",
+      topic: "In his 11th I-day speech, PM Modi flags ‘secular civil code’,women’s safety, exudes pragmatism on Bangladesh",
+      content: "In his 11th successive Independence Day address to the nation from the ramparts of the Red Fort, Prime Minister Narendra Modi gave a renewed push to the BJP’s ideological aim of a Uniform Civil Code, reframing the idea as a “secular civil code” enshrined in the Constitution and a way of ensuring equality before law for all.",
+      author: "Iron Man",
+      date: "12th April 2024"
+    },
+    {
+      imgUrl: "https://th-i.thgim.com/public/incoming/8y78id/article68527890.ece/alternates/LANDSCAPE_1200/PTI08_15_2024_000033B.jpg",
+      topic: "In his 11th I-day speech, PM Modi flags ‘secular civil code’,women’s safety, exudes pragmatism on Bangladesh",
+      content: "In his 11th successive Independence Day address to the nation from the ramparts of the Red Fort, Prime Minister Narendra Modi gave a renewed push to the BJP’s ideological aim of a Uniform Civil Code, reframing the idea as a “secular civil code” enshrined in the Constitution and a way of ensuring equality before law for all.",
+      author: "Iron Man",
+      date: "12th April 2024"
+    },
+    {
+      imgUrl: "https://th-i.thgim.com/public/incoming/8y78id/article68527890.ece/alternates/LANDSCAPE_1200/PTI08_15_2024_000033B.jpg",
+      topic: "In his 11th I-day speech, PM Modi flags ‘secular civil code’,women’s safety, exudes pragmatism on Bangladesh",
+      content: "In his 11th successive Independence Day address to the nation from the ramparts of the Red Fort, Prime Minister Narendra Modi gave a renewed push to the BJP’s ideological aim of a Uniform Civil Code, reframing the idea as a “secular civil code” enshrined in the Constitution and a way of ensuring equality before lawJP’s ideological aim of a Uniform Civil Code, reframing the idea as a “secular civil code” enshrined in the Constitution and a way of ensuring equality before lawJP’s ideological aim of a Uniform Civil Code, reframing the idea as a “secular civil code” enshrined in the Constitution and a way of ensuring equality before lawJP’s ideological aim of a Uniform Civil Code, reframing the idea as a “secular civil code” enshrined in the Constitution and a way of ensuring equality before lawJP’s ideological aim of a Uniform Civil Code, reframing the idea as a “secular civil code” enshrined in the Constitution and a way of ensuring equality before lawJP’s ideological aim of a Uniform Civil Code, reframing the idea as a “secular civil code” enshrined in the Constitution and a way of ensuring equality before law for all.",
+      author: "Iron Man",
+      date: "12th April 2024"
+    }
+  ]
   return (
     <>
-      <div className="">
-        <div className="flex mb-5 gap-5 border-black border-2 p-4 rounded-xl hover:bg-slate-200">
-          <div className="">
-            <img
-              src="https://th-i.thgim.com/public/incoming/8y78id/article68527890.ece/alternates/LANDSCAPE_1200/PTI08_15_2024_000033B.jpg"
-              alt="Modi"
-              className="w-100 "
-            />
-          </div>
-          <div className="flex-col mb-">
-            <h2 className="font-bold">
-              In his 11th I-day speech, PM Modi flags ‘secular civil code’,
-              women’s safety, exudes pragmatism on Bangladesh
-            </h2>
-            <p className="flex gap-2 m-0 font-normal">
-              <a className="text-gray-800">Iron man</a>
-              <time className=" text-gray-400">2024-08-15 19:10</time>
-            </p>
-            <p>
-              In his 11th successive Independence Day address to the nation from
-              the ramparts of the Red Fort, Prime Minister Narendra Modi gave a
-              renewed push to the BJP’s ideological aim of a Uniform Civil Code,
-              reframing the idea as a “secular civil code” enshrined in the
-              Constitution and a way of ensuring equality before law for all.
-              
-            </p>
-          </div>
-        </div>
-        <div className="flex mb-5 gap-5  border-black border-2 p-4 rounded-xl hover:bg-slate-200">
-          <div className="">
-            <img
-              src="https://th-i.thgim.com/public/incoming/8y78id/article68527890.ece/alternates/LANDSCAPE_1200/PTI08_15_2024_000033B.jpg"
-              alt="Modi"
-              className="w-100"
-            />
-          </div>
-          <div className="flex-col">
-            <h2 className="font-bold">
-              In his 11th I-day speech, PM Modi flags ‘secular civil code’,
-              women’s safety, exudes pragmatism on Bangladesh
-            </h2>
-            <p>
-              In his 11th successive Independence Day address to the nation from
-              the ramparts of the Red Fort, Prime Minister Narendra Modi gave a
-              renewed push to the BJP’s ideological aim of a Uniform Civil Code,
-              reframing the idea as a “secular civil code” enshrined in the
-              Constitution and a way of ensuring equality before law for all.
-            </p>
-          </div>
-        </div>
-        <div className="flex mb-5 gap-5  border-black border-2 p-4 rounded-xl hover:bg-slate-200">
-          <div className="">
-            <img
-              src="https://th-i.thgim.com/public/incoming/8y78id/article68527890.ece/alternates/LANDSCAPE_1200/PTI08_15_2024_000033B.jpg"
-              alt="Modi"
-              className="w-100"
-            />
-          </div>
-          <div className="flex-col gap-">
-            <h2 className="font-bold">
-              In his 11th I-day speech, PM Modi flags ‘secular civil code’,
-              women’s safety, exudes pragmatism on Bangladesh
-            </h2>
-            <p>
-              In his 11th successive Independence Day address to the nation from
-              the ramparts of the Red Fort, Prime Minister Narendra Modi gave a
-              renewed push to the BJP’s ideological aim of a Uniform Civil Code,
-              reframing the idea as a “secular civil code” enshrined in the
-              Constitution and a way of ensuring equality before law for all.
-            </p>
-          </div>
-        </div>
-        <div className="flex mb-5 gap-5  border-blue-950   border-2 p-4 rounded-xl hover:bg-slate-500">
-          <div className="">
-            <img
-              src="https://th-i.thgim.com/public/incoming/8y78id/article68527890.ece/alternates/LANDSCAPE_1200/PTI08_15_2024_000033B.jpg"
-              alt="Modi"
-              className="w-100"
-            />
-          </div>
-          <div className="flex-col ">
-            <h2 className="font-bold">
-              In his 11th I-day speech, PM Modi flags ‘secular civil code’,
-              women’s safety, exudes pragmatism on Bangladesh
-            </h2>
-            <p>
-              In his 11th successive Independence Day address to the nation from
-              the ramparts of the Red Fort, Prime Minister Narendra Modi gave a
-              renewed push to the BJP’s ideological aim of a Uniform Civil Code,
-              reframing the idea as a “secular civil code” enshrined in the
-              Constitution and a way of ensuring equality before law for all.
-            </p>
-          </div>
-        </div>
+      <div className=" px-3 lg:p-10">
+        {
+          blogs.map((blog, index) => (
+            <div key={index} className="flex mb-5 gap-5 bg-gray-100 border-gray-400 border p-4 my-10 rounded-md flex-col md:flex-row">
+              <div className="flex items-center min-w-64 md:min-w-[400px] lg:min-w-[450px] xl:min-w-[500px]">
+                <img
+                  src={blog.imgUrl}
+                  alt="Modi"
+                  className=""
+                />
+              </div>
+              <div className="flex-col">
+                <h2 className=" text-3xl py-3"> {blog.topic} </h2>
+                <h3 className="text-right py-2 text-gray-600"> {blog.author}, {blog.date} </h3>
+                <p className="h-32 overflow-y-auto p-3 my-5" > {blog.content} </p>
+              </div>
+            </div>
+          ))
+        }
       </div>
     </>
   );
