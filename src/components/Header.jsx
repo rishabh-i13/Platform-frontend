4047a56466aa151415f9d9ccab1981abaa28e77b
Rishabh Shukla <rishabhshukla1333@gmail.com>
2024-09-01 15:51:50 +0530

    header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,41 +1,61 @@
 import React, { useEffect, useState } from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
 
-
 const Header = () => {
+  const [username, setUsername] = useState('');
 
-  const [username,setUsername]=useState('');
-
-  useEffect(()=>{
-    fetch('http://localhost:4000/profile',{
-      credentials :'include',
-    }).then(response=>{
-      response.json().then(userInfo=>{
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
         setUsername(userInfo.username);
-      })
-    })
-  })
+      });
+    });
+  }, []);
 
   return (
-      <header className="flex justify-between bg-blue-300 h-15 w-full fixed top-0 z-50">
-        <Link to="" className='font-bold text-3xl  m-2'>
-          PLATFORM
-        </Link>
-        <nav className="flex gap-[15px] m-2">
-          {username && (
-            <>
-            <Link to="/create" className="text-inherit flex items-center text-white bg-black p-2 px-4 rounded-md cursor-pointer hover:bg-blue-100 hover:text-black duration-1000 hover:scale-110">Create <FiPlus /></Link>
-            </>
-          )}
-          {!username && (
-            <>
-            <Link to="/login" className="text-inherit flex items-center text-white bg-black p-2 px-4 rounded-md cursor-pointer hover:bg-blue-100 hover:text-black duration-1000 hover:scale-110"> Login</Link>
-            <Link to="/register" className="text-inherit flex items-center text-white bg-black p-2 rounded-md cursor-pointer hover:bg-blue-100 hover:text-black duration-1000 hover:scale-110">Register</Link>
-            </>
-          )}
-          </nav>
-      </header>
+    <header className="flex justify-between bg-blue-300 h-15 w-full fixed top-0 z-50">
+      <Link to="" className="font-bold text-3xl  m-2">
+        PLATFORM
+      </Link>
+      <nav className="flex gap-[15px] m-2">
+        {username && (
+          <>
+            <Link
+              to="/create"
+              className="text-inherit flex items-center text-white bg-black p-2 px-4 rounded-md cursor-pointer hover:bg-blue-100 hover:text-black duration-1000 hover:scale-110"
+            >
+              Create <FiPlus />
+            </Link>
+            <Link
+              to="/create"
+              className="text-inherit flex items-center text-white bg-black p-2 px-4 rounded-md cursor-pointer hover:bg-blue-100 hover:text-black duration-1000 hover:scale-110"
+            >
+              LogOut
+            </Link>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link
+              to="/login"
+              className="text-inherit flex items-center text-white bg-black p-2 px-4 rounded-md cursor-pointer hover:bg-blue-100 hover:text-black duration-1000 hover:scale-110"
+            >
+              {" "}
+              Login
+            </Link>
+            <Link
+              to="/register"
+              className="text-inherit flex items-center text-white bg-black p-2 rounded-md cursor-pointer hover:bg-blue-100 hover:text-black duration-1000 hover:scale-110"
+            >
+              Register
+            </Link>
+          </>
+        )}
+      </nav>
+    </header>
   );
 };
 
