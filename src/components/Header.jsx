5e25aeec71b0643250002fa8d4624e11b9805ae3
Rishabh Shukla <rishabhshukla1333@gmail.com>
2024-09-08 15:44:19 +0530

    login-register

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,25 +3,34 @@ import { Link } from "react-router-dom";
 import { FiPlus } from "react-icons/fi";
 
 const Header = () => {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
 
   useEffect(() => {
     fetch("http://localhost:4000/profile", {
       credentials: "include",
     }).then((response) => {
       response.json().then((userInfo) => {
-        setUsername(userInfo.username);
+        // console.log(userInfo)
+        setEmail(userInfo.email);
       });
     });
   }, []);
 
+  function logout(){
+    fetch("http://localhost:4000/profile", {
+      credentials:'include',
+      method:'POST',
+    });
+    setEmail(null);
+
+  }
   return (
     <header className="flex justify-between bg-blue-300 h-15 w-full fixed top-0 z-50">
       <Link to="" className="font-bold text-3xl  m-2">
         PLATFORM
       </Link>
       <nav className="flex gap-[15px] m-2">
-        {username && (
+        {email && (
           <>
             <Link
               to="/create"
@@ -30,20 +39,19 @@ const Header = () => {
               Create <FiPlus />
             </Link>
             <Link
-              to="/create"
+              onClick={logout}
               className="text-inherit flex items-center text-white bg-black p-2 px-4 rounded-md cursor-pointer hover:bg-blue-100 hover:text-black duration-1000 hover:scale-110"
             >
               LogOut
             </Link>
           </>
         )}
-        {!username && (
+        {!email && (
           <>
             <Link
               to="/login"
               className="text-inherit flex items-center text-white bg-black p-2 px-4 rounded-md cursor-pointer hover:bg-blue-100 hover:text-black duration-1000 hover:scale-110"
             >
-              {" "}
               Login
             </Link>
             <Link
